236fb3b126f518a85e4616d21c6e0f784f5bb3c9
Artavazd833 <atanesyan1983@gmail.com>
2019-08-29 14:39:12 +0400

    Esorva coomit

---
+++ tour.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+
+ * Template Name: Tour
+
+ */
+ get_header() ?>
+<div class='main_wraper'> 
+    <section id='tr_tppSec'>
+        <div class='tr_top_img img1'> <img src='<?php echo get_template_directory_uri().'/assets/images/e.png'?>'></div>
+        <div class='tr_top_img img2'><img src='<?php echo get_template_directory_uri().'/assets/images/h.png'?>'></div>
+        <div class='tr_top_img img3'><img src='<?php echo get_template_directory_uri().'/assets/images/o.png'?>'></div>
+        <div class='tr_top_img img4'><img src='<?php echo get_template_directory_uri().'/assets/images/m.png'?>'></div>
+        <div class='tr_top_img img5'><img src='<?php echo get_template_directory_uri().'/assets/images/e2.png'?>'></div>
+        <div class='tr_top_content'>
+            <div class='tr_top_right'></div>
+            <div class='tr_top_imgHome'><img src='<?php echo get_template_directory_uri().'/assets/images/home.png'?>'></div>
+        </div>
+    </section>
+</div>
+
+
+
+
+
+
+
+<?php get_footer() ?>
+
+
+
+
+
+
