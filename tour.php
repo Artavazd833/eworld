75842bd42db2d8286325873fbce83230bd67bf67
Artavazd833 <atanesyan1983@gmail.com>
2019-08-29 17:54:55 +0400

    new update

--- tour.php
+++ tour.php
@@ -8,15 +8,25 @@
  get_header() ?>
 <div class='main_wraper'> 
     <section id='tr_tppSec'>
-        <div class='tr_top_img img1'> <img src='<?php echo get_template_directory_uri().'/assets/images/e.png'?>'></div>
-        <div class='tr_top_img img2'><img src='<?php echo get_template_directory_uri().'/assets/images/h.png'?>'></div>
-        <div class='tr_top_img img3'><img src='<?php echo get_template_directory_uri().'/assets/images/o.png'?>'></div>
-        <div class='tr_top_img img4'><img src='<?php echo get_template_directory_uri().'/assets/images/m.png'?>'></div>
-        <div class='tr_top_img img5'><img src='<?php echo get_template_directory_uri().'/assets/images/e2.png'?>'></div>
+        <div class='tr_topBG_imgWraper'>
+                <div class='tr_top_img img1'> <img src='<?php echo get_template_directory_uri().'/assets/images/e.png'?>'></div>
+                <div class='tr_top_img img2'><img src='<?php echo get_template_directory_uri().'/assets/images/h.png'?>'></div>
+                <div class='tr_top_img img3'><img src='<?php echo get_template_directory_uri().'/assets/images/o.png'?>'></div>
+                <div class='tr_top_img img4'><img src='<?php echo get_template_directory_uri().'/assets/images/m.png'?>'></div>
+                <div class='tr_top_img img5'><img src='<?php echo get_template_directory_uri().'/assets/images/e2.png'?>'></div>
+        </div>
+        
         <div class='tr_top_content'>
             <div class='tr_top_right'></div>
-            <div class='tr_top_imgHome'><img src='<?php echo get_template_directory_uri().'/assets/images/home.png'?>'></div>
+            <div class='tr_top_imgHome'>
+                <div class='tr_top_right_cont'>
+                   <div class='tr_top_right_vertical'>here we go</div>
+                   <div class='tr_top_right_position'>see what's happening inside</div> 
+                </div>
+                <img src='<?php echo get_template_directory_uri().'/assets/images/home.png'?>'>
+            </div>
         </div>
+     
     </section>
 </div>
 
